35c5258729779835882dadcc14840e1ec5b6bcda
Gavin Sinclair <gsinclair@gmail.com>
2010-05-19 18:45:03 +1000

    test/realistic_example.rb: fleshed out

--- test/realistic_example.rb
+++ test/realistic_example.rb
@@ -1,6 +1,4 @@
 
-require 'attest/auto'
-
 # This file contains a realistic example of unit testing using attest.  The
 # code contains bugs which the tests reveal.
 
@@ -19,3 +17,78 @@ require 'attest/auto'
 # - something that takes a nested data structure (arrays inside hashes
 #   inside...) and generates meaningful summary data
 # - look at existing unit testing tutorials on the net
+#   - found nothing
+# - something in standard library ?
+#   - extend Date with some Duration-type methods ?
+# - some extensions to core classes, taken from 'extensions' ?
+
+  # p1 = Point.new(5, -2)
+  # p2 = Point.polar(11, 30)
+  # Point[:A] = p1
+  # Point[:B] = p2
+  # Point[:A]         # -> p1
+class Point
+  @@index = Hash.new
+  def initialize(x, y)
+    @x, @y = x, y
+  end
+  def Point.cartesian(x, y)
+    Point.new(x, y)
+  end
+  def Point.polar(r, th)
+    Point.new(r * Math.cos(th), r * Math.sin(th))
+  end
+  def Point.[](symbol)
+    @@index[symbol]
+  end
+  def Point.[]=(symbol, point)
+    @@index[symbol] = point
+  end
+  def polar
+    r = Math.sqrt(x**2 + y**2)
+    th = Math.atan2(y, x)
+    [r, th]
+  end
+end
+
+
+# 
+# Simple markup function to turn formatted text into HTML:
+# * Implement _word_, *word* and @word@ (em, strong and code, respectively);
+# * Surround paragraphs with <p> tags;
+# * Turn single newlines into spaces.
+# 
+def simple_markup(string)
+  tag = lambda { |str, t| "<#{t}>#{str}</#{t}>" }
+  html = string.
+    gsub(/_(\w+)_/)     { tag[$1, :em]     }.
+    gsub(/\*(\W+)\*/)   { tag[$1, :strong] }.
+    gsub(/@(\w+)@/)     { tag[$1, :code]   }.
+    gsub(/(.+?)\n{2,}/) { tag[$1, :p]      }. 
+    gsub(/\n/, ' ')
+end
+
+D "simple_markup" do
+  D "breaks text into paragraphs" do
+    Eq simple_markup("abc"),                     "<p>abc</p>"
+    Eq simple_markup("abc\n\ndef"),              "<p>abc</p><p>def</p>"
+    Eq simple_markup("abc\n\ndef\n\n\n\nghi\n"), "<p>abc</p><p>def</p><p>ghi</p>"
+  end
+  D "replaces single newlines with a space" do
+    Eq simple_markup("abc\ndef"),      "<p>abc xyz</p>"
+    Eq simple_markup("abc\ndef\nghi"), "<p>abc xyz ghi</p>"
+  end
+  D "handles mixed paragraphs and single newlines correctly" do
+    text = "Once upon a time\nIn a land far away\n\n" \
+         + "A frog named Kermit\n\nDecared he was here to stay"
+    html = "<p>Once upon a time In a land far away</p>" \
+         + "<p>A frog named Kermit Decared he was here to stay</p>"
+    Eq simple_markup(text), html
+  end
+  D "handles _words_" do
+    Eq simple_markup("One _two_ _three_"), "<p>One <em>two</em> <em>three</em></p>"
+  end
+  D "handles *words*" do
+    Eq simple_markup("One *fine* day"), 
+  end
+end
