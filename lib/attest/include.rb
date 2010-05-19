767171702281668c5b71a17d9db4d10bb4e2e327
Gavin Sinclair <gsinclair@gmail.com>
2010-05-19 18:48:09 +1000

    lib/attest/include.rb: nifty shortcut

---
+++ lib/attest/include.rb
@@ -0,0 +1,14 @@
+
+# A convenient way to include the Attest methods like T, Eq, etc. in the top
+# level.
+#
+# Long way:
+#   require 'attest'
+#   include Attest
+#
+# Short way:
+#   require 'attest/include'
+#
+
+require 'attest'
+inclide Attest
